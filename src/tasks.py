e7a0fb16bc4a36c3d77f7ea15cfe9dc2d360bd8c
Romain Gayon <romaing@google.com>
2025-05-27 16:40:00 +0200

    Entropy (#1)
    * init

--- src/tasks.py
+++ src/tasks.py
@@ -1,38 +1,73 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import csv
+import io
 import math
 
-import subprocess
-
 from openrelik_worker_common.file_utils import create_output_file
-from openrelik_worker_common.task_utils import create_task_result, get_input_files
+from openrelik_worker_common.reporting import Report
+from openrelik_worker_common.task_utils import create_task_result
+from openrelik_worker_common.task_utils import get_input_files
 
 from .app import celery
 
 # Task name used to register and route the task to the correct queue.
 TASK_NAME = "openrelik-worker-entropy.tasks.entropy"
 
+# Default value for high entropy
+HIGH_ENTROPY_THRESHOLD = 7.0
+
 # Task metadata for registration in the core system.
 TASK_METADATA = {
-    "display_name": "openrelik-worker-entropy",
-    "description": "Calculate entropy of files",
+    "display_name": "High Entropy",
+    "description": "Detect files with entropy.",
     # Configuration that will be rendered as a web for in the UI, and any data entered
     # by the user will be available to the task function when executing (task_config).
     "task_config": [
+        {
+            "name": "threshold",
+            "label": "Entropy threshold value",
+            "description": f"Entropy threshold value. (default is {HIGH_ENTROPY_THRESHOLD})",
+            "type": "string",
+            "require": False,
+        }
     ],
 }
 
-def calculate_entropy(data):
+
+def calculate_entropy(data: bytes) -> float:
+    """Calculate entropy of data, as a number of bits of entropy per byte.
+
+    Args:
+      data: The data to calculate entropy on.
+
+    Returns:
+      A float representing the entropy.
+    """
     entropy = 0
     if not data:
         return 0
     for x in range(256):
         p_x = float(data.count(x)) / len(data)
         if p_x > 0:
-            entropy += - p_x * math.log(p_x, 2)
+            entropy += -p_x * math.log(p_x, 2)
     return entropy
 
 
 @celery.task(bind=True, name=TASK_NAME, metadata=TASK_METADATA)
-def command(
+def run_entropy_task(
     self,
     pipe_result: str = None,
     input_files: list = None,
@@ -40,7 +75,7 @@ def command(
     workflow_id: str = None,
     task_config: dict = None,
 ) -> str:
-    """Run <REPLACE_WITH_COMMAND> on input files.
+    """Run Entropy task on input files.
 
     Args:
         pipe_result: Base64-encoded result from the previous Celery task, if any.
@@ -54,15 +89,47 @@ def command(
     """
     input_files = get_input_files(pipe_result, input_files or [])
 
-    for input_file in input_files:
-        # Run the command
-        with open(input_file.path, "w") as fh:
-            entropy = calculate_entropy(fh.read())
+    high_entropy_files = []
 
+    high_entropy_thershold = task_config.get("threshold", HIGH_ENTROPY_THRESHOLD)
+
+    string_io = io.StringIO()
+    csv_writer = csv.DictWriter(string_io, fieldnames=['path', 'entropy'])
+    csv_writer.writeheader()
+
+    for input_file in input_files:
+        with open(input_file.get("path"), "rb") as fh:
+            filename = input_file.get("display_name")
+            entropy = calculate_entropy(fh.read())
+            csv_writer.writerow({'path': filename, 'entropy': str(entropy)})
+            if entropy >= HIGH_ENTROPY_THRESHOLD:
+                high_entropy_files.append([filename, entropy])
+
+    csv_result = create_output_file(
+        output_path,
+        display_name="entropy_results",
+        extension=".csv",
+        data_type="openrelik:entropy:results",
+    )
+    with open(csv_result.path, "w") as outfile:
+        outfile.write(string_io.getvalue())
+
+    task_report = Report("Entropy analyzer report")
+    results_summary = (
+        f"Found {len(high_entropy_files)} files "
+        f"with high entropy (>{HIGH_ENTROPY_THRESHOLD})."
+    )
+    task_report.summary = results_summary
+    summary_section = task_report.add_section()
+    summary_section.add_paragraph(results_summary)
+
+    details_section = task_report.add_section()
+    for path, entropy in high_entropy_files:
+        details_section.add_bullet(f'{path}: {entropy}', level=1)
 
     return create_task_result(
-        output_files=output_files,
         workflow_id=workflow_id,
-        command=base_command_string,
+        output_files=[csv_result.to_dict()],
+        task_report=task_report.to_dict(),
         meta={},
     )
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,12 +1,32 @@
-"""Tests tasks."""
+"""Tests Entropy tasks."""
 
-# Note: Use pytest for writing tests!
-import pytest
+from src.tasks import calculate_entropy, HIGH_ENTROPY_THRESHOLD
 
-# from src.tasks import command
+class TestEntropyTask:
+    """Tests for the Entropy task."""
 
-def test_task_command():
-    """Test command task."""
+    def test_random_entropy(self):
+        with open('test_data/random.1k', 'rb') as random_file:
+            entropy = calculate_entropy(random_file.read())
+            assert (entropy > 7.8)
 
-    ret = "some dummy return value"
-    assert isinstance(ret,str)
+    def test_zero_entropy(self):
+        with open('test_data/zero.1k', 'rb') as random_file:
+            entropy = calculate_entropy(random_file.read())
+            assert (entropy == 0.0)
+
+    def test_log_entropy(self):
+        with open('test_data/syslog', 'rb') as random_file:
+            entropy = calculate_entropy(random_file.read())
+            assert (entropy == 5.129738750791151)
+
+    def test_binary_low_entropy(self):
+        with open('test_data/hello.bin', 'rb') as random_file:
+            entropy = calculate_entropy(random_file.read())
+            assert (entropy > 0 )
+            assert (entropy < HIGH_ENTROPY_THRESHOLD)
+
+    def test_binary_high_entropy(self):
+        with open('test_data/hello-upx.bin', 'rb') as random_file:
+            entropy = calculate_entropy(random_file.read())
+            assert (entropy > HIGH_ENTROPY_THRESHOLD)
